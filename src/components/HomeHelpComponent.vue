6187b3b4fe82e802061a23c7a52f4f97c371d50b
Matt Miller <thisismattmiller@gmail.com>
2021-05-27 09:26:42 -0400

    manual videos

--- src/components/HomeHelpComponent.vue
+++ src/components/HomeHelpComponent.vue
@@ -3,9 +3,20 @@
 
 
     <div style="padding:1em;">
-    <h1>Beta Quick Start Video</h1>
-    <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/lYCWSghcQok" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-    <hr>
+    <h1>Quick Start Video</h1>
+    <p>These quick videos show how to get started using the editor, click to expand the section:</p>
+<!--     <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/lYCWSghcQok" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <hr> -->
+
+    <details>
+      <summary>
+        The home screens.
+      </summary>  
+      <video controls width="75%">
+        <source src="https://github.com/lcnetdev/bfe2/blob/master/manual/videos/bfe2_help_home_screens.webm?raw=true" type="video/webm"/>
+      </video>
+    </details>
+    
     </div>
 
 
